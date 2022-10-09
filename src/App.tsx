0156f6542a6dee61c2ab580e5563e1cd1deea786
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2022-10-08 19:07:26 -0500

    homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/homepage/Homepage';
 import LoginPage from './pages/login/Login';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path={"/"} element={<Home />} /> */}
+        <Route path={'/home'} element={<HomePage />} />
         <Route path={'/login'} element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
