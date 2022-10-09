fc9c03a33e299c4bf40604f01ab364d145ae2042
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2022-10-09 08:01:18 -0500

    SEND NOTifications working with PUSH

--- src/pages/artist/send-notification.tsx
+++ src/pages/artist/send-notification.tsx
@@ -7,8 +7,13 @@ import {
   Button,
 } from '@mantine/core';
 import { useForm } from '@mantine/form';
+import { useContractFunction } from '@usedapp/core';
+import { Contract, utils } from 'ethers';
+import React from 'react';
+import PNSArtists from '../../assets/abis/PNSArtists.json';
 
 export default function SendNotification() {
+  const [description, setDescription] = React.useState('');
   const form = useForm({
     initialValues: {
       name: '',
@@ -21,6 +26,15 @@ export default function SendNotification() {
       email: (value) => !/^\S+@\S+$/.test(value),
       subject: (value) => value.trim().length === 0,
     },
+  });
+
+  const epnsContract = '0x3416f37595c745637c3e1eeb390003654a3bb864';
+  const wethInterface = new utils.Interface(PNSArtists.abi);
+  const contract = new Contract(epnsContract, wethInterface) as any;
+
+  const { state, send } = useContractFunction(contract, 'notify', {
+    transactionName: 'Wrap',
+    gasLimitBufferPercentage: 10,
   });
 
   return (
@@ -44,11 +58,18 @@ export default function SendNotification() {
         autosize
         name="message"
         variant="filled"
-        {...form.getInputProps('subject')}
+        // {...form.getInputProps('subject')}
+        onChange={(e) => setDescription(e.target.value)}
       />
 
       <Group position="center" mt="xl">
-        <Button type="submit" size="md">
+        <Button
+          type="submit"
+          size="md"
+          onClick={() => {
+            send('MUSIQUETH', description);
+          }}
+        >
           Send message
         </Button>
       </Group>
